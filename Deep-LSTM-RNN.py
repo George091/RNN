c767518839b902f070a1db2ee034daab4ee61dbe
George091 <Georgebernardbarker@gmail.com>
2019-11-21 20:05:02 -0500

    Create Deep-LSTM-RNN.py

---
+++ Deep-LSTM-RNN.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 20:03:08 2019
+
+@author: georgebarker andrezeromski
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow_datasets as tfds
+import tensorflow as tf
+  
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True,
+                          as_supervised=True)
+
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+encoder = info.features['text'].encoder
+  
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.padded_batch(BATCH_SIZE, train_dataset.output_shapes)
+
+test_dataset = test_dataset.padded_batch(BATCH_SIZE, test_dataset.output_shapes)
+
+model = tf.keras.Sequential([
+tf.keras.layers.Embedding(encoder.vocab_size, 64),
+tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, dropout=.3, return_sequences=True)),
+tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+tf.keras.layers.Dense(64, activation='relu'),
+tf.keras.layers.Dense(1, activation='sigmoid')
+])
+    
+model.compile(loss='binary_crossentropy',
+              optimizer=tf.keras.optimizers.Adam(1e-4),
+              metrics=['accuracy'])
+
+history = model.fit(train_dataset, epochs=10,
+                    validation_data=test_dataset, 
+                    validation_steps=30)
+
+test_loss, test_acc = model.evaluate(test_dataset)
+
+print('Test Loss: {}'.format(test_loss))
+print('Test Accuracy: {}'.format(test_acc))
