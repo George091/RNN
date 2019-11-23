8c31d45c865fde7d1e4dfe2bab291b1f1cf86485
George091 <Georgebernardbarker@gmail.com>
2019-11-22 21:08:35 -0500

    Update LSTM-RNN.py

--- LSTM-RNN.py
+++ LSTM-RNN.py
@@ -14,6 +14,8 @@ from keras.layers import LSTM
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras.layers import Dropout
+from keras.layers import Bidirectional, GlobalMaxPool1D
+
 
 # load the dataset but only keep the top n words, zero the rest
 top_words = 8000
@@ -23,13 +25,16 @@ max_review_length = 500
 X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
 X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
 # create the model
-embedding_vecor_length = 10
+embedding_vector_length = 128
 model = Sequential()
-model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
+model.add(Embedding(top_words, embedding_vector_length, input_length=max_review_length))
+
+model.add(Bidirectional(LSTM(64, return_sequences = True)))
+model.add(GlobalMaxPool1D())
 model.add(Dropout(0.3))
-model.add(LSTM(100))
-model.add(Dropout(0.3))
-model.add(Dense(1, activation='tanh'))
+model.add(Dense(20, activation="relu"))
+model.add(Dense(1, activation="sigmoid"))
+
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 model.fit(X_train, y_train, epochs=3, batch_size=64)
